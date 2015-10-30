b6aef39c84e1ef11e482dc1e3de631e3c026200e
Gustavo Cardoso <gustavocardoso@gmail.com>
2015-10-30 00:43:47 -0200

    Add individual volume track.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140201164804) do
+ActiveRecord::Schema.define(version: 20151028150700) do
 
   create_table "tracks", force: true do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140201164804) do
     t.string   "file_content_type"
     t.integer  "file_file_size"
     t.datetime "file_updated_at"
+    t.integer  "volume"
   end
 
 end
